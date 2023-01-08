9eec2e2248524d29d1f5d7fa7b8674395f0b5f61
rdeluna801 <rdeluna801@byui.edu>
2023-01-08 08:36:25 -0700

    changed menu format

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,49 +1,23 @@
-$.fn.doubleTapToGo = function (params) {
-  if (
-    !("ontouchstart" in window) &&
-    !navigator.msMaxTouchPoints &&
-    !navigator.userAgent.toLowerCase().match(/windows phone os 7/i)
-  )
-    return false;
-  this.each(function () {
-    var curItem = false;
-    $(this).on("click", function (e) {
-      var item = $(this);
-      if (item[0] != curItem[0]) {
-        e.preventDefault();
-        curItem = item;
-      }
-    });
-    $(document).on("click touchstart MSPointerDown", function (e) {
-      var resetItem = true,
-        parents = $(e.target).parents();
-      for (var i = 0; i < parents.length; i++)
-        if (parents[i] == curItem[0]) resetItem = false;
-      if (resetItem) curItem = false;
-    });
-  });
-  return this;
+const hambutton = document.querySelector(".ham");
+const mainnav = document.querySelector(".navigation");
+
+hambutton.addEventListener(
+  "click",
+  () => {
+    mainnav.classList.toggle("responsive");
+  },
+  false
+);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {
+  if (window.innerWidth > 760) mainnav.classList.remove("responsive");
 };
-jQuery, window, document;
-$("#nav li:has(ul)").doubleTapToGo();
-/*
-	VIEWPORT BUG FIX
-	iOS viewport scaling bug fix, by @mathias, @cheeaun and @jdalton
-*/
-(function (e) {
-  function o() {
-    s.content =
-      "width=device-width,minimum-scale=" + i[0] + ",maximum-scale=" + i[1];
-    e.removeEventListener(n, o, true);
-  }
-  var t = "addEventListener",
-    n = "gesturestart",
-    r = "querySelectorAll",
-    i = [1, 1],
-    s = r in e ? e[r]("meta[name=viewport]") : [];
-  if ((s = s[s.length - 1]) && t in e) {
-    o();
-    i = [0.25, 1.6];
-    e[t](n, o, true);
-  }
-})(document);
+
+/*** Programming Notes **************************************
+  Arrow Functions - es6 syntactically compact alternative to a regular function expression
+  see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+  or https://www.w3schools.com/js/js_arrow_function.asp
+
+  classList property - https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+  */
